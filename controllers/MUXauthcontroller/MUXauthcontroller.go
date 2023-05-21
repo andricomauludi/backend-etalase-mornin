e6b7bdeeaf45ce5d9fa42538de3cb97e07fffefb
andricomauludi <mauludi21@gmail.com>
2023-05-21 21:33:32 +0700

    change login with mux to login to gin, Create User with JWT

---
+++ controllers/MUXauthcontroller/MUXauthcontroller.go
@@ -0,0 +1,47 @@
+package MUXauthcontroller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/andricomauludi/backend-etalase-mornin/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(w http.ResponseWriter, l *http.Request) {
+
+}
+func Register(w http.ResponseWriter, l *http.Request) { //mengambil input json yg diterima
+
+	var userInput models.User
+	decoder := json.NewDecoder(l.Body)
+	if err := decoder.Decode(&userInput); err != nil {
+		log.Fatal("Failed to decode json")
+	}
+	defer l.Body.Close()
+
+	//hash password menggunakan bcrypt
+
+	//membuat hashpassword dengan input password dari user dan bcrypt
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	// memasukkan password yg diinput ke dalam models
+	userInput.Password = string(hashPassword)
+
+	//insert ke db
+	if err := models.DB.Create(&userInput).Error; err != nil {
+		log.Fatal("Failed to save data")
+	}
+
+	//respon json
+	response, _ := json.Marshal(map[string]string{"message": "success to insert"})
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
+}
+func Logout(w http.ResponseWriter, l *http.Request) {
+
+}
+
+//test rebase commit
--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"github.com/andricomauludi/backend-etalase-mornin/controllers/authcontroller"
 	"github.com/andricomauludi/backend-etalase-mornin/controllers/productcontroller"
+	"github.com/andricomauludi/backend-etalase-mornin/initializers"
+	"github.com/andricomauludi/backend-etalase-mornin/models"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
 	r := gin.Default()
-	// l := mux.NewRouter()
 
-	// models.ConnectDatabase() //melakukan koneksi database
+	models.ConnectDatabase() //melakukan koneksi database
 
 	// //URL CONTROLLER DAN FUNCTIONNYA
 
-	// l.HandleFunc("login", authcontroller.Login).Methods("POST")
-	// l.HandleFunc("register", authcontroller.Register).Methods("POST")
-	// l.HandleFunc("logout", authcontroller.Logout).Methods("POST")
-
-	// log.Fatal(http.ListenAndServe(":8080", l))
-
+	//AUTH
+	r.POST("api/signup", authcontroller.Signup)
+	//CRUD Product
 	r.GET("api/products", productcontroller.Index)
 	r.GET("api/products/:id", productcontroller.Show) //terdapat id yg params nya dapat diambil oleh controller
 	r.POST("api/products", productcontroller.Create)
@@ -26,4 +29,5 @@ func main() {
 	r.DELETE("api/products", productcontroller.Delete)
 
 	r.Run() //WAJIB ADA agar controller terjalani
+	// log.Fatal(http.ListenAndServe(":8080", l))
 }
