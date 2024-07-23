95f2f1a2bbe7b147d4b99566180aefe5c408f2ad
andricomauludi <mauludi21@gmail.com>
2024-07-24 00:53:50 +0700

    change into 3000 frontend

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -147,7 +147,7 @@ func stringToIntSlice(str string) []int {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://103.175.217.209:3000")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://20.195.42.22:3003")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://20.195.42.22:3000")
 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
